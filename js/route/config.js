585136fb9f13a1887b2619f482d42aee221962d1
Pєρє <P>
2016-12-31 03:27:21 -0300

    ng-route implemented

---
+++ js/route/config.js
@@ -0,0 +1,15 @@
+angular.module("toDoList",["ui.router"])
+
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state("home",{
+                url: "/",
+                templateUrl: "Templates/home.html",
+                controller: "toDoListCtrl"
+            })
+            .state("contact",{
+                url: "/contact",
+                templateUrl: "Templates/contact.html"
+            })
+    }]);
